3160226128678e196b779c45bc978244e1b04716
ninima0323 <ninima0323@gmail.com>
2022-03-11 20:37:43 +0900

    초기 ios 프로젝트 생성

---
+++ VelCheck/VelCheck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VelCheck
+//
+//  Created by 김나현 on 2022/03/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
